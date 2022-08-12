1e31801a48b13a6eb79197214546985d9fe219df
Ana Valeska Santos de Souza <avaleska2908@hotmail.com>
2022-08-11 21:14:47 -0300

    feat: building the user model.

---
+++ src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "../server.js";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+});
+
+const User = mongoose.model( 'User', UserSchema );
+
+export default User;
